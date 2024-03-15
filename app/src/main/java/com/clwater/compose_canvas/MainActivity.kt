011bbb30bb43d8ffdd3fee5de1b8deed2031b1f2
clwater <gzb@clwater.top>
2024-03-15 10:29:13 +0800

    wip(menu): refix menu

--- app/src/main/java/com/clwater/compose_canvas/MainActivity.kt
+++ app/src/main/java/com/clwater/compose_canvas/MainActivity.kt
@@ -3,12 +3,17 @@ package com.clwater.compose_canvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.clwater.compose_canvas.bezier.BezierActivity
 import com.clwater.compose_canvas.clap.ClapActivity
@@ -19,7 +24,7 @@ import com.clwater.compose_canvas.ui.theme.AndroidComposeCanvasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        ShapeActivity.start(this@MainActivity)
+//        ShapeActivity.start(this@MainActivity)
         super.onCreate(savedInstanceState)
         setContent {
             AndroidComposeCanvasTheme {
@@ -30,24 +35,114 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(
                         modifier = Modifier.padding(12.dp),
-                        verticalArrangement = Arrangement.Center
                     ) {
-                        Button(onClick = { Canvas1Activity.start(this@MainActivity) }) {
-                            Text(text = "Sun Moon")
+                        Row(modifier = Modifier.height(100.dp)) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { Canvas1Activity.start(this@MainActivity) }) {
+                                Text(text = "Sun Moon")
+                            }
+                            Image(
+                                modifier = Modifier.weight(2f),
+                                painter = painterResource(id = R.drawable.sun_moon),
+                                contentDescription = ""
+                            )
                         }
 
-                        Button(onClick = { BezierActivity.start(this@MainActivity) }) {
-                            Text(text = "Bezier")
+                        Spacer(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+
+                        Row(modifier = Modifier.height(100.dp)) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { BezierActivity.start(this@MainActivity) }) {
+                                Text(text = "Bezier")
+                            }
+                            Image(
+                                modifier = Modifier.weight(2f),
+                                painter = painterResource(id = R.drawable.bezier),
+                                contentDescription = ""
+                            )
                         }
-                        Button(onClick = { ClapActivity.start(this@MainActivity) }) {
-                            Text(text = "Clap")
+                        Spacer(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+
+                        Row(modifier = Modifier.height(100.dp)) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { ClapActivity.start(this@MainActivity) }) {
+                                Text(text = "Clap")
+                            }
+                            Image(
+                                modifier = Modifier
+                                    .weight(2f)
+                                    .align(Alignment.CenterVertically),
+                                painter = painterResource(id = R.drawable.icon_hand_fill),
+                                contentDescription = ""
+                            )
                         }
-                        Button(onClick = { TreeActivity.start(this@MainActivity) }) {
-                            Text(text = "Tree")
+                        Spacer(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+
+                        Row(modifier = Modifier.height(100.dp)) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { TreeActivity.start(this@MainActivity) }) {
+                                Text(text = "Tree")
+                            }
+                            Image(
+                                modifier = Modifier.weight(2f),
+                                painter = painterResource(id = R.drawable.tree),
+                                contentDescription = ""
+                            )
                         }
-                        Button(onClick = { ShapeActivity.start(this@MainActivity) }) {
-                            Text(text ="Shape")
+                        Spacer(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+
+                        Row(modifier = Modifier.height(100.dp)) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { ShapeActivity.start(this@MainActivity) }) {
+                                Text(text = "Shape")
+                            }
+                            Image(
+                                modifier = Modifier.weight(2f),
+                                painter = painterResource(id = R.drawable.shape),
+                                contentDescription = ""
+                            )
                         }
+                        Spacer(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
                     }
                 }
             }
